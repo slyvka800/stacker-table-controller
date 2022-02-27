bbb652517c3da3a028430c72871d6e3323a2179c
slyvka800 <p.a.slyvka@gmail.com>
2022-02-27 10:04:50 +0200

    Menu selection doesn't work

--- Bluetooth connection app/PeripheralMenuController.swift
+++ Bluetooth connection app/PeripheralMenuController.swift
@@ -7,12 +7,22 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
-extension ViewController: NSCollectionViewDelegate, NSCollectionViewDataSource {
+protocol MenuControllerDelegate{
+    func selectedMenuItem(indexPath: IndexPath)
+}
+
+extension ViewController: NSCollectionViewDataSource, NSCollectionViewDelegate, MenuControllerDelegate {
     
     func setupCollectionView() {
+        peripheralsMenuCollectionView.isSelectable = true
+        peripheralsMenuCollectionView.allowsEmptySelection = true
+        peripheralsMenuCollectionView.allowsMultipleSelection = false
+        peripheralsMenuCollectionView.register(PeripheralMenuItem.self, forItemWithIdentifier: PeripheralMenuItem.reuseIdentfier)
         peripheralsMenuCollectionView.delegate = self
         peripheralsMenuCollectionView.dataSource = self
+        
     }
     
     func numberOfSections(in collectionView: NSCollectionView) -> Int {
@@ -29,6 +39,7 @@ extension ViewController: NSCollectionViewDelegate, NSCollectionViewDataSource {
         
         if foundPeripherals.indices.contains(setIndex) {
             menuItem.peripheralNameLabel.stringValue = foundPeripherals[setIndex].name ?? "unknown"
+            menuItem.menuController = self
             if foundPeripherals[setIndex] != peripheral {
                 menuItem.peripheralConnectionStatus.isHidden = true
             }
@@ -36,5 +47,37 @@ extension ViewController: NSCollectionViewDelegate, NSCollectionViewDataSource {
         return menuItem
     }
     
+    func selectedMenuItem(indexPath: IndexPath) {
+        print("kek")
+        guard let selectedPeripheral = getFoundPeripheral(index: indexPath.item)
+        else { return }
+        print(selectedPeripheral.name)
+        centralManager.connect(selectedPeripheral, options: nil)
+    }
     
+    func toggleConnectionIndicator(peripheral: CBPeripheral, isConnected: Bool) {
+        let numOfSections = peripheralsMenuCollectionView.numberOfSections
+        
+        (0..<numOfSections).forEach { aSection in
+            (0..<peripheralsMenuCollectionView.numberOfItems(inSection: aSection)).forEach { anItem in
+                let indexPath = IndexPath(item: anItem, section: aSection)
+                if let item = peripheralsMenuCollectionView.item(at: indexPath), let castedItem = item as? PeripheralMenuItem {
+                    castedItem.peripheralConnectionStatus.isHidden = !isConnected
+                }
+            }
+        }
+        
+    }
+}
+
+//extension NSTableView {
+//    open override func validateProposedFirstResponder(_ responder: NSResponder, for event: NSEvent?) -> Bool {
+//        return true
+//    }
+//}
+
+class KEK: NSTableView {
+    override func validateProposedFirstResponder(_ responder: NSResponder, for event: NSEvent?) -> Bool {
+        return true
+    }
 }
--- Bluetooth connection app/PeripheralMenuItem.swift
+++ Bluetooth connection app/PeripheralMenuItem.swift
@@ -12,11 +12,32 @@ class PeripheralMenuItem: NSCollectionViewItem {
     @IBOutlet var peripheralNameLabel: NSTextField!
     @IBOutlet var peripheralConnectionStatus: ReusableView!
     
+    var menuController: MenuControllerDelegate?
+    
     static let reuseIdentfier = NSUserInterfaceItemIdentifier("PeripheralMenuItem")
+    
+    override var isSelected: Bool {
+        didSet {
+            print(oldValue)
+            print(isSelected)
+            maanageSelectedState()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        view.wantsLayer = true
+//        self.collectionView?.isSelectable = true
+//        self.collectionView?.allowsEmptySelection = true
+//        self.collectionView?.allowsMultipleSelection = false
+    }
+    
+    private func maanageSelectedState() {
+        print(isSelected)
+        if !isSelected {
+            guard let indexPath = self.collectionView?.indexPath(for: self) else { return }
+            menuController?.selectedMenuItem(indexPath: indexPath)
+        }
     }
     
 }
